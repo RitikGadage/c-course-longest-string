99defe2c3e58479acfcfcabf0a43bea6fd26b32c
RitikGadage <ritzgadage12345@gmail.com>
2022-05-12 17:22:00 +0530

    add longest_string.c

---
+++ longest_string.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+    int longestStringIndex;
+    if (argc <= 1)
+    {
+        puts("");
+    }
+    else
+    {
+        longestStringIndex = 1;
+        for (int currentStringIndex = 2; currentStringIndex < argc; currentStringIndex++)
+        {
+            if (strlen(argv[longestStringIndex]) < strlen(argv[currentStringIndex]))
+            {
+                longestStringIndex = currentStringIndex;
+            }
+        }
+        puts(argv[longestStringIndex]);
+    }
+    return EXIT_SUCCESS;
+}
+
+
+
